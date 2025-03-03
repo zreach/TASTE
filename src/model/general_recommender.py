933a1720f200d43c63536dcaa5985135186ba922
zreach <1103134401@qq.com>
2025-03-03 23:31:59 +0800

    add

--- src/model/general_recommender.py
+++ src/model/general_recommender.py
@@ -33,26 +33,90 @@ class GeneralRecommender(nn.Module):
 
         self.use_audio = config['use_audio']
         self.use_text = config['use_text']
-        self.content_feature_type = config['content_feature_type']
-        self.content_embedding_size = config['content_embedding_size']
+        self.wav_feature_type = config['wav_feature_type']
+        self.wav_embedding_size = config['wav_embedding_size']
         
         if self.use_audio is None:
             self.use_audio = False
         if self.use_text is None:
             self.use_text = False
-        if self.content_feature_type is None:
-            self.content_feature_type = 'CLAP' #TODO 设置默认参数
+        if self.wav_feature_type is None:
+            self.wav_feature_type = 'CLAP' #TODO 设置默认参数
         
         if self.use_audio:
             wav_feat_path = config['wav_feat_path']
             with open(wav_feat_path, 'rb') as fp:
-                    music_features_array = pickle.load(fp)
-        
-        if self.use_text:
-            wav_feat_path = config['wav_feat_path']
-            with open(wav_feat_path, 'rb') as fp:
-                    music_features_array = pickle.load(fp)
+                wav_features_array = pickle.load(fp)
+            wav_features_array['[PAD]'] = np.zeros((1, self.wav_embedding_size))
+            music_features = torch.zeros((len(self.token2id['tracks_id']), self.wav_embedding_size ))
+            
+            for k, v in self.token2id['tracks_id'].items():
+                self.id2token[v] = k
+                music_features[v] = torch.Tensor(wav_features_array[k])
 
+            self.id2afeat = nn.Embedding.from_pretrained(music_features)
+            self.id2afeat.requires_grad_(False)
+
+            size_list = [
+                self.wav_embedding_size 
+            ] + config['wav_mlp_sizes'] + [self.embedding_size]
+            self.wav_mlp = MLPLayers(size_list, 0.2)
+            self.num_feature_field += 1
+
+        if self.use_text:
+            text_feat_path = config['text_feat_path']
+            with open(text_feat_path, 'rb') as fp:
+                    text_features_array = pickle.load(fp)
+            text_features_array['[PAD]'] = np.zeros((1, self.text_embedding_size))
+            text_features = torch.zeros((len(self.token2id['tracks_id']), self.text_embedding_size ))
+            
+            for k, v in self.token2id['tracks_id'].items():
+                self.id2token[v] = k
+                text_features[v] = torch.Tensor(text_features_array[k])
+
+            self.id2tfeat = nn.Embedding.from_pretrained(text_features)
+            self.id2tfeat.requires_grad_(False)
+            size_list = [
+                self.text_embedding_size 
+            ] + config['text_mlp_sizes'] + [self.embedding_size]
+            self.text_mlp = MLPLayers(size_list, 0.2)
+            self.num_feature_field += 1
+
+
+        for field_name in self.field_names:
+            if field_name == self.LABEL:
+                continue
+            if dataset.field2type[field_name] == "token":
+                self.token_field_names.append(field_name)
+                self.token_field_dims.append(dataset.num(field_name))
+            
+            elif (
+                dataset.field2type[field_name] == "float"
+                and field_name in self.numerical_features
+            ):
+                self.float_field_names.append(field_name)
+                self.float_field_dims.append(dataset.num(field_name))
+            else:
+                continue
+
+            self.num_feature_field += 1
+        
+        if len(self.token_field_dims) > 0:
+            self.token_field_offsets = np.array(
+                (0, *np.cumsum(self.token_field_dims)[:-1]), dtype=np.long
+            )
+            self.token_embedding_table = FMEmbedding(
+                self.token_field_dims, self.token_field_offsets, self.embedding_size
+            )
+        if len(self.float_field_dims) > 0:
+            self.float_field_offsets = np.array(
+                (0, *np.cumsum(self.float_field_dims)[:-1]), dtype=np.long
+            )
+            self.float_embedding_table = FLEmbedding(
+                self.float_field_dims, self.float_field_offsets, self.embedding_size
+            )
+
+        self.first_order_linear = FMFirstOrderLinear(config, dataset)
 
     def __str__(self):
         """
